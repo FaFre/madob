7b788b4cf84f904013a0db46a43479ade136c1ec
Fabian Freund <mail@fabi.online>
2020-01-19 22:25:23 +0100

    added example

--- lib/example/example.dart
+++ lib/example/example.dart
@@ -1,1 +1,40 @@
-void main() async {}
+import 'package:hive_managed/example/data/objects/data/entities/managed_task_model.dart';
+import 'package:hive_managed/hive_managed.dart';
+import 'package:hive_managed/src/helper/path_provider.dart';
+
+import 'data/objects/data/entities/managed_project_model.dart';
+import 'data/objects/data/entities/project_model.dart';
+import 'data/objects/data/entities/task_model.dart';
+
+void main() async {
+  final directory = await PathProvider().getTempDirectory();
+
+  HiveRepository.init(directory.path);
+  HiveRepository.register('projectBox', ProjectAdapter());
+  HiveRepository.register('taskBox', TaskAdapter());
+
+  final taskid = StrongUuid().generate();
+  final projectId = StrongUuid().generate();
+
+  final task = ManagedTask();
+  await task.initialize(() => Task(taskid));
+
+  await task.setTitle('Test Task');
+  print('After .setTitle(): ${await task.title}');
+
+  await task.setProject(Project(projectId));
+  print(
+      'Id of project after .setProject(): ${(await task.project).managedKey}');
+
+  //Close both boxes
+  await HiveRepository.closeBox<Task>();
+  await HiveRepository.closeBox<Project>();
+
+  //Set title through closed task
+  await (await task.project).setTitle('Test Project');
+
+  final regainedProject = ManagedProject();
+  await regainedProject.initialize(() => Project(projectId));
+
+  print('title of project set through task: ${await regainedProject.title}');
+}
