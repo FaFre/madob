ddd49064b5de79a518330104026f6727396f0b00
Fabian Freund <mail@fabi.online>
2020-02-19 23:45:30 +0100

    copied implementation istead of extend to avoid irritating generator

--- madob_generator/lib/src/annotations/madob_type.dart
+++ madob_generator/lib/src/annotations/madob_type.dart
@@ -1,7 +1,23 @@
 import 'package:meta/meta.dart';
 import 'package:hive/hive.dart';
 
-class MadobType extends HiveType {
-  const MadobType({@required int typeId, String adapterName})
-      : super(typeId: typeId, adapterName: adapterName);
+class MadobType {
+  /// The typeId of the annotated class.
+  final int typeId;
+
+  /// The name of the generated adapter.
+  //TODO: Pass it to HiveType at generation
+  final String adapterName;
+
+  /// This parameter can be used to keep track of old fieldIds which must not
+  /// be reused. The generator will throw an error if a legacy fieldId is
+  /// used again.
+  // final List<int> legacyFieldIds;
+
+  /// If [adapterName] is not set, it'll be `"YourClass" + "Adapter"`.
+  const MadobType({
+    @required this.typeId,
+    this.adapterName,
+    //this.legacyFieldIds,
+  });
 }
