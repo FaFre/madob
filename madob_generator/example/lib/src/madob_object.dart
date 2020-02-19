cc6748d5e67544cab1193f810bcaec34160d28e4
Fabian Freund <mail@fabi.online>
2020-02-19 23:40:19 +0100

    made example generatable; removd sample code

--- madob_generator/example/lib/src/madob_object.dart
+++ madob_generator/example/lib/src/madob_object.dart
@@ -1,7 +1,8 @@
 import 'package:madob/madob.dart';
 import 'package:madob_generator/madob_generator.dart';
+import 'package:hive/hive.dart';
 
-// ignore: uri_has_not_been_generated
+part 'madob_object.madob_generator.g.dart';
 part 'madob_object.g.dart';
 
 @MadobType(typeId: 0)
@@ -14,36 +15,3 @@ abstract class IProject implements IKey {
   @MadobSetter(1)
   Future<void> setTitle(String newTitle);
 }
-
-// class ManagedProject extends Madob<Project> implements IProject {
-//   @override
-//   String get managedKey => hiveObject.managedKey;
-
-//   @override
-//   Future<void> setTitle(String newTitle) =>
-//       setValue((project) => project.setTitle(newTitle));
-
-//   @override
-//   Future<String> get title => getValue((project) => project.title);
-// }
-
-// @HiveType(typeId: 0)
-// class Project extends HiveObject implements IProject {
-//   @HiveField(0)
-//   final String _id;
-
-//   @override
-//   String get managedKey => _id;
-
-//   @HiveField(1)
-//   String _title;
-
-//   @override
-//   Future<String> get title => Future.value(_title);
-//   @override
-//   Future<void> setTitle(String newTitle) async {
-//     _title = newTitle;
-//   }
-
-//   Project(this._id) : assert(_id != null && _id.isNotEmpty);
-// }
