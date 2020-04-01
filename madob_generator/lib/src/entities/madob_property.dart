2f30ce55dd051002f93f9ad838ca2cbfd3599e77
Fabian Freund <mail@fabi.online>
2020-04-01 19:14:08 +0200

    enhanced code generation for references

--- madob_generator/lib/src/entities/madob_property.dart
+++ madob_generator/lib/src/entities/madob_property.dart
@@ -2,15 +2,30 @@ import 'package:madob/madob.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
+import '../scheme/madob_schemes.dart';
+
 /// Contains information about a property
 /// to generate a future setter/getter for [HiveObject] and [Madob]
 class MadobProperty {
   /// [Type]-name of the property
   final String type;
 
-  /// Name of the referenced [HiveObject]
-  final String referencedHiveObjectName;
-  bool get isReferenced => referencedHiveObjectName?.isNotEmpty == true;
+  /// Name of the referenced class.
+  /// Could be a [HiveObject] or abstract class for generation
+  final String referencedClass;
+
+  /// Name of the referenced class, converted to a generated [HiveObject]-name
+  /// in case a abstract class is given
+  String get referencedHiveObject =>
+      hasInterfaceNamingConvention(referencedClass)
+          ? referencedClass.substring(1)
+          : referencedClass;
+
+  /// Returns if the property is referenced to another Object
+  bool get isReferenced => (referencedClass?.isNotEmpty == true);
+
+  /// If the reference pointing to a Madob-Object
+  final bool isReferenceManaged;
 
   /// Getter-Name of the property
   final String getName;
@@ -27,5 +42,6 @@ class MadobProperty {
       @required this.getName,
       @required this.setName,
       @required this.setParameterName,
-      this.referencedHiveObjectName});
+      this.referencedClass,
+      this.isReferenceManaged});
 }
